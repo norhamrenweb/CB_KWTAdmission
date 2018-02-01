09b52c5a31914e8ffb030f2a47289b3392438ab2
Josepe04 <Josepe04@ucm.es>
2018-02-01 12:51:07 +0100

    Escritura archivos okey

--- src/cb_kwtadmission/CB_KWTAdmission.java
+++ src/cb_kwtadmission/CB_KWTAdmission.java
@@ -5,6 +5,8 @@
  */
 package cb_kwtadmission;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -33,38 +35,60 @@ public class CB_KWTAdmission {
             //if the difference is 1 add the student id in a array
             //loop the array to get the phone number of the parent
             // send sms and write a message in a textfile
-            
-            
+
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection cn = DriverManager.getConnection("jdbc:sqlserver://deb-qat.odbc.renweb.com:1433;databaseName=deb_qat","DEB_QAT_CUST","UnderQuiet+227");
-            
+            Connection cn = DriverManager.getConnection("jdbc:sqlserver://deb-qat.odbc.renweb.com:1433;databaseName=deb_qat", "DEB_QAT_CUST", "UnderQuiet+227");
+
             Statement st = cn.createStatement();
             ArrayList<Integer> studentids = new ArrayList<>();
+            ArrayList<DateTime> dates = new ArrayList<>();
             ResultSet rs = st.executeQuery("Select td.id,td.note as assesdate,si.studentid as studentid from trackingdata td inner join studentinquiry si on td.id=si.inquiryid where td.trackingsystemid = 7 and itemnumber = 1");
-            while(rs.next())
-            {
+            while (rs.next()) {
                 DateTime assesDate = new DateTime(rs.getString("assesdate"));
                 DateTime today = new DateTime();
                 Days d = Days.daysBetween(assesDate, today);
-            int days = d.getDays();
-                 if(days==1)
-                 {
-                     studentids.add(rs.getInt("studentid"));
-                 }
+                int days = d.getDays();
+                if (days == 1) {
+                    studentids.add(rs.getInt("studentid"));
+                    dates.add(assesDate);
+                }
             }
-            for(Integer s:studentids)
-            {
-            ResultSet rs1 = st.executeQuery("Select CellPhone from Students where studentid = " + s);
-            while(rs1.next())
-            {
-                String cell = rs1.getString("cellphone");
-                //send the message and write in teh text file, also write if the cell phone was not found
+            for (int i = 0; i <studentids.size();i++) {
+                ResultSet rs1 = st.executeQuery("Select CellPhone from Students where studentid = " + studentids.get(i));
+                if (rs1.next()) {
+                    String cell = rs1.getString("cellphone");
+                    File f = new File("cellphonefound.log");
+                    FileWriter fichero = null;
+                    try {
+                        fichero = new FileWriter(f.getAbsoluteFile(),true);
+                            // Escribimos linea a linea en el fichero
+                        fichero.write("Cell phone message data: "
+                                + "Cellphone="+cell
+                                + " IDstudent="+ studentids.get(i) + " Date="
+                                + dates.get(i)+ System.getProperty("line.separator"));
+                        fichero.close();
+                    } catch (Exception ex) {
+                        System.out.println("Mensaje de la excepción: " + ex.getMessage());
+                    }
+                }else{
+                    File f = new File("cellphonenotfound.log");
+                    FileWriter fichero = null;
+                    try {
+                        fichero = new FileWriter(f.getAbsoluteFile(),true);
+                            // Escribimos linea a linea en el fichero
+                        fichero.write("Cell phone not found: "
+                                + "IDstudent="+ studentids.get(i) + " Date="
+                                + dates.get(i)+ System.getProperty("line.separator"));
+                        fichero.close();
+                    } catch (Exception ex) {
+                        System.out.println("Mensaje de la excepción: " + ex.getMessage());
+                    }
+                }
             }
-            }
-            } catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex) {
             Logger.getLogger(CB_KWTAdmission.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
 }
